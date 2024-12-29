01aa4637271d8772d4cd57f03c8de742bc17445d
葉君緯 <A112280042@gm1.usc.edu.tw>
2024-12-30 00:00:36 +0800

    Delete ToyCode/CPE☆ directory

--- ToyCode/CPE☆/uva10055 - Hashmat the Brave Warrior.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(){
-	long long x,y;
-	while(scanf("%lld %lld",&x,&y)!=EOF){
-		printf("%lld\n",abs(x-y));
-	}	
-}
--- ToyCode/CPE☆/uva10071 - Back to High School Physics.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-	int u,v;
-	while(scanf("%d %d",&u,&v)!=EOF){
-		printf("%d\n",2*u*v);
-	}
-}
